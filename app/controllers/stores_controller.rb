c8c073cfbe54afe2b156bc2418d0708c2d5b0fab
Jason Pelletier <jpelletier19@me.com>
2016-08-05 11:39:51 -0500

    can view all locations and all products

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -10,21 +10,21 @@ end
 
 # UPDATE
 put "/stores/:id" do
-  # Send a put request to update a store 
+  # Send a put request to update a store
   # Redirect to that stores show page
 end
 
 # Index
-get '/stores' do 
+get '/stores' do
 	@stores = Store.all
 
 	erb(:"stores/index")
 end
 
 # Show
-get '/stores/:id' do  
-  # Get a store and show the show page
-
+get '/stores/:id' do
+  @store = Store.find(params[:id])
+  erb(:"stores/show")
 end
 
 # EDIT
@@ -33,7 +33,7 @@ get "/stores/:id/edit" do
 end
 
 # Delete
-delete '/stores/:id' do 
+delete '/stores/:id' do
   # Find a store and destroy it!
-  # Redirect back to /stores 
+  # Redirect back to /stores
 end
