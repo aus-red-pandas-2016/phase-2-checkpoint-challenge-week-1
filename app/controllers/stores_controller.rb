7b8c7707192a4abd2ef43c90c28434f233fb5cc3
CJ Jessett <email": null>
2016-08-05 11:03:58 -0500

    release 1

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,6 +1,13 @@
+# Index
+get '/stores' do
+  @stores = Store.all
+
+  erb :"stores/index"
+end
+
 # NEW
 get "/stores/new" do
-
+  erb :'stores/new'
 end
 
 # Create
@@ -8,23 +15,17 @@ post "/stores" do
   # Create a new Store and then send em to that stores show page
 end
 
+# Show
+get '/stores/:id' do
+  # Get a store and show the show page
+  @store = Store.find_by_id params[:id]
+  erb :'stores/show'
+end
+
 # UPDATE
 put "/stores/:id" do
-  # Send a put request to update a store 
+  # Send a put request to update a store
   # Redirect to that stores show page
-end
-
-# Index
-get '/stores' do 
-	@stores = Store.all
-
-	erb(:"stores/index")
-end
-
-# Show
-get '/stores/:id' do  
-  # Get a store and show the show page
-
 end
 
 # EDIT
@@ -33,7 +34,7 @@ get "/stores/:id/edit" do
 end
 
 # Delete
-delete '/stores/:id' do 
+delete '/stores/:id' do
   # Find a store and destroy it!
-  # Redirect back to /stores 
+  # Redirect back to /stores
 end
