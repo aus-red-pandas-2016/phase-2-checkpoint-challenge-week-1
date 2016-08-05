6efe0825ffce0b9691fb95ceafecf37814fed4ab
Jessica Bohn <bohnjessica15@gmail.com>
2016-08-05 10:53:38 -0500

    Add AR assocations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,4 @@
 class Location < ActiveRecord::Base
   belongs_to :store, { presence: true }
+  has_many :products, through: :location_products, foreign_key: "product_id"
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,3 @@
 class Product < ActiveRecord::Base
-
+  has_many :locations, through: :location_products, foreign_key: "location_id"
 end
